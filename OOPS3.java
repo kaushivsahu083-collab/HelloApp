f83958cfbf4a42fc85b3a4b4e434b1dbd5b762d4
Test User <test@example.com>
2026-02-17 11:26:49 +0530

    Added OOPS3.java to UC-3

---
+++ OOPS3.java
@@ -0,0 +1,14 @@
+public class OOPS3 {
+    public static void main(String[] args) {
+
+        String output = String.join("\n",
+                " *****   *****   ****    ****  ",
+                "*     * *     *  *   *  *      ",
+                "*     * *     *  ****    ***    ",
+                "*     * *     *  *           *  ",
+                " *****   *****   *      ****    "
+        );
+
+        System.out.println(output);
+    }
+}
