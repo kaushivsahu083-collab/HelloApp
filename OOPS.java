4cdb6fa575d7f3adc549a35737f87c1ca92d8947
Test User <test@example.com>
2026-02-17 10:13:29 +0530

    Initial commit - UC-1

---
+++ OOPS.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class OOPS {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+    
+}
---
+++ OOPS2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class OOPS2 {
+    public static void main(String[] args) {
+       
+        System.out.println("   **   " + "   " + "   **   " + "   " + " **********   " + "   *********");
+        System.out.println(" **  ** " + "   " + " **  ** " + "   " + " **      **   " + " **         ");
+        System.out.println("*      *" + "   " + "*      *" + "   " + " **      **   " + "*           ");
+        System.out.println("*      *" + "   " + "*      *" + "   " + " **      **   " + "*           ");
+        System.out.println("*      *" + "   " + "*      *" + "   " + " **********   " + " ********   ");
+        System.out.println("*      *" + "   " + "*      *" + "   " + " **********   " + "         ** ");
+        System.out.println("*      *" + "   " + "*      *" + "   " + " **           " + "           *");
+        System.out.println("*      *" + "   " + "*      *" + "   " + " **           " + "           *");
+        System.out.println(" **  ** " + "   " + " **  ** " + "   " + " **           " + "         ** ");
+        System.out.println("   **   " + "   " + "   **   " + "   " + " **           " + "*********   ");
+    }
+
+}
+    
